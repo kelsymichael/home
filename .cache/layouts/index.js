91e879c47fc2e3c4488a24c6867c2b6c92599e4b
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-08-20 15:41:52 -0600

    Setup stylus plugin

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/kelsymichael/Dropbox/projects/portfolio/home/src/layouts/index.js"
-  import data from "/Users/kelsymichael/Dropbox/projects/portfolio/home/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
---
+++ home/src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () =>
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+    <code>`<h1> hellow word</h1>`</code>    
+  </div>
+
+export default SecondPage
